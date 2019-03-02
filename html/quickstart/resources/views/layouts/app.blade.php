f51e0b58b85a6e815ae65d338af74b17f20c80e8
Ishigetani <k.subaru1230@gmail.com>
2019-03-02 13:14:07 +0900

    add JQuery

--- html/quickstart/resources/views/layouts/app.blade.php
+++ html/quickstart/resources/views/layouts/app.blade.php
@@ -79,6 +79,7 @@
     </div>
 
     <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 
