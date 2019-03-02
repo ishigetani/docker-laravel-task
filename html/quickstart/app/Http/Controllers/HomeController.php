723a6dce6ee6503b7d03d7df273054bdc7ce5088
Ishigetani <k.subaru1230@gmail.com>
2019-03-02 13:49:14 +0900

    処理をControlerに移動

--- html/quickstart/app/Http/Controllers/HomeController.php
+++ html/quickstart/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class HomeController extends Controller
 {
@@ -17,12 +18,52 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * タスク一覧
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', compact('tasks'));
+    }
+
+    /**
+     * タスク追加
+     * 
+     * @return redirect
+     */
+    public function add(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            // バリデーションエラー
+            return redirect()
+                ->route('home')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * タスク削除
+     * 
+     * @return redirect
+     */
+    public function delete(Task $task)
+    {
+        $task->delete();
+
+        return redirect()->route('home');
     }
 }
--- html/quickstart/routes/web.php
+++ html/quickstart/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,41 +13,10 @@ use App\Task;
 |
 */
 
-Route::get('/', function () {
-    // タスク一覧
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-Route::post('/task', function (Request $request) {
-    // タスク追加
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255'
-    ]);
-
-    if ($validator->fails()) {
-        // バリデーションエラー
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-Route::delete('/task/{task}', function (Task $task) {
-    $task->delete();
-
-    return redirect('/');
-});
+Route::redirect('/', '/login');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/home', 'HomeController@add')->name('task.add');
+Route::delete('/home', 'HomeController@delete')->name('task.delete');
